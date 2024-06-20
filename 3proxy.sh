5ad519ca00bfa1cab8a6e986e9e070818fa152ba
9nazaebashu <40621139+Nazaren0k@users.noreply.github.com>
2024-06-20 18:35:09 +0300

    Update 3proxy.sh

--- 3proxy.sh
+++ 3proxy.sh
@@ -11,27 +11,27 @@ cp bin/3proxy /usr/bin/
 
 # Настройка конфигурационного файла 3proxy.cfg с помощью команды tee
 sudo tee <<EOF >/dev/null /etc/3proxy/3proxy.cfg
-nserver 8.8.8.8  # DNS-сервер Google
-nserver 8.8.4.4  # DNS-сервер Google
-nserver 1.1.1.1  # DNS-сервер Cloudflare
-nserver 1.0.0.1  # DNS-сервер Cloudflare
+nserver 8.8.8.8
+nserver 8.8.4.4
+nserver 1.1.1.1
+nserver 1.0.0.1
 
-nscache 65536  # Размер кэша DNS запросов
+nscache 65536
 
-timeouts 1 5 30 60 180 1800 15 60  # Таймауты соединений
+timeouts 1 5 30 60 180 1800 15 60
 
-daemon  # Запуск 3proxy в режиме демона
+daemon
 
-log /var/log/3proxy/3proxy.log D  # Настройка логирования в файл /var/log/3proxy/3proxy.log
-logformat "- +_L%t.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"  # Формат записей лога
+log /var/log/3proxy/3proxy.log D
+logformat "- +_L%t.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"
 
-auth strong  # Настройка строгой авторизации
+auth strong
 
-allow * * * 80-88,8080-8088 HTTP  # Разрешение доступа для HTTP портов
-allow * * * 443,8443 HTTPS  # Разрешение доступа для HTTPS портов
+allow * * * 80-88,8080-8088 HTTP
+allow * * * 443,8443 HTTPS
 
-proxy -p53129 -n -a  # Настройка прокси-сервера с портом 53129 и параметрами -n (без DNS запросов) и -a (аутентификация всех пользователей)
-users user:CL:P@ssv0rd  # Добавление пользователей с указанными учетными данными
+proxy -p53129 -n -a
+users user:CL:P@ssv0rd
 
 EOF
 
